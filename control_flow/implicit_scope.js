043316a226ce37bf8139a4e643bdfa0129bfb6f0
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-08-17 07:40:03 +0530

    "Implicit scope"

---
+++ control_flow/implicit_scope.js
@@ -0,0 +1,7 @@
+// Note:
+// here we are having a implicit scope, which means we are assuming there is already a scope
+// its not a good way to write code.
+
+let balance = 4000;
+
+if (balance < 5000) console.log(`Balance is sufficient`)
