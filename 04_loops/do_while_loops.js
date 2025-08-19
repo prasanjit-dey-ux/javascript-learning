949a7af6e04703e9a31ea4c7992f4fd7f9d7f58a
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-08-19 23:56:06 +0530

    added do_while loop

---
+++ 04_loops/do_while_loops.js
@@ -0,0 +1,23 @@
+// while loop
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++
+}
+
+let arr = ["house", "car", "bike"]
+
+let index = 0
+while (index < arr.length) {
+    console.log(arr[index]);
+    index ++
+}
+
+// do while
+
+do {
+    let i = 0
+    i++
+} while (i> 5);
+
