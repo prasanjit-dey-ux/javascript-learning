68cd0d7ef90019abd8aff8d83a3149e8caa83df4
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-08-23 22:45:44 +0530

    reduce added example

--- 04_loops/reduce.js
+++ 04_loops/reduce.js
@@ -2,11 +2,40 @@
 
 let bag = [1, 2, 3, 4, 5]
 
-const val = bag.reduce((acumilator, currentvalue) => {
-        console.log(`acumilator: ${acumilator} currentvalue: ${currentvalue}`);
-        let sum = acumilator + currentvalue
+const val = bag.reduce((accumilator, currentvalue) => {
+        console.log(`acumilator: ${accumilator} currentvalue: ${currentvalue}`);
+        let sum = accumilator + currentvalue
         return sum;
     }, 0 )
 
 console.log(val);
+
+// shopping cart
+
+let cart = [{
+    name: "Burger",
+    price: 300,
+    quantity: 3
+},
+{
+    name: "Pizza",
+    price: 400,
+    quantity: 2
+},
+{
+    name: "Moito",
+    price: 100,
+    quantity: 3
+}
+];
+
+let discountCoupon = 50;
+
+const itemAdd = cart.reduce((acc, item) =>{
+    let price = acc + item.price * item.quantity;
+    return price
+}, 0);
+
+console.log(itemAdd);
+
 
