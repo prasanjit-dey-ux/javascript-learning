6863272758fec77abbd2d9ca6e45c9e42b3b1daf
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-08-29 19:50:45 +0530

    modifieng element assignment

--- 05_Dom/modifyingElement.js
+++ 05_Dom/modifyingElement.js
@@ -1,3 +1,13 @@
-const greeting = document.querySelector('#greeting');
+const user = {
+    name: "Jiiit",
+    username: "@jiit_ui",
+    bio: "Software developer and coffee enthusiast. Building cool things for the web."
+};
 
-console.log(greeting.textContent);
+const nameElement = document.querySelector("#user-name");
+const usernameElement = document.querySelector('#user-username');
+const bio = document.querySelector('#user-bio');
+
+nameElement.textContent = user.name;
+usernameElement.textContent = user.username;
+bio.textContent = user.bio;
