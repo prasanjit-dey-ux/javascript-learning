22d663e09046f6a907ed683607261153e3d93a38
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-05 18:41:09 +0530

    event loop phase

---
+++ 07_asynchronous/eventLoopPhase.js
@@ -0,0 +1,25 @@
+// Understanding the order of execution
+console.log("1 - Start");
+
+// Macrotask (Time phase)
+setTimeout(() => console.log('2 - setTimeout'),0);
+
+// Microtask 
+Promise.resolve().then(() => console.log('3 - Promise'));
+
+// Immidiate execution
+console.log('4 - Synchronous');
+
+// Microtask
+queueMicrotask(() => console.log('5 - queueMicrotask'));
+
+// Next tick (highest priority microtask in Node.js)
+process.nextTick(() => console.log('6 - NextTick')) 
+
+// Output order:
+// 1 - Start
+// 4 - Synchronous  
+// 6 - nextTick (highest priority)
+// 3 - Promise (microtask)
+// 5 - queueMicrotask (microtask)
+// 2 - setTimeout (macrotask)
