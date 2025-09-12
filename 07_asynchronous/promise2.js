6d934fc33e37d1897315605236fb2e398feb8b29
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-09-12 19:17:55 +0530

    updated

--- 07_asynchronous/promise2.js
+++ 07_asynchronous/promise2.js
@@ -1,14 +1,14 @@
-// All three network requests start at the same time
+
 Promise.all([
     fetchPosts(),
     fetchFriendsList(),
     fetchAdvertisements()
 ])
 .then(([posts, friends, ads]) => {
-    // This code runs only after ALL three promises have fulfilled
+
     renderPage(posts, friends, ads);
 })
 .catch(error => {
-    // If any one of the requests fails, this will catch it
+
     showPageLoadError(error);
 });
