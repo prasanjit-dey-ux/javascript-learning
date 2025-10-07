5b3c8f110cadfa51b69dc8506471e1e0c77fae5a
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-07 23:14:10 +0530

    added async await

---
+++ 07_asynchronous/asyncawait.js
@@ -0,0 +1,19 @@
+/**
+ * What is async?
+ - Example:  
+ async function getData(){
+    // return promise
+ }
+ - The difference between this and normal fnc is it will always return promise
+ - we can either return a promise from this function or if we return a value for eg,
+   return "WELCOME", what it will do it will wrap this value in promise and return us a promise.
+
+
+
+ * What is await?
+ * How async function work behind the scene?
+ * Example of using async/await?
+ * Error Handling
+ * Interviews
+ * Async await vs Promise.then/.catch
+ */
